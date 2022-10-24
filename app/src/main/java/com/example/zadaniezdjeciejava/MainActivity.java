ee4bf15f2ebacaf207a74a97b7b55cd429968a90
Kurtyka03 <kurtyka03@gmail.com>
2022-10-24 15:48:53 +0200

    add Transparency funnction

--- app/src/main/java/com/example/zadaniezdjeciejava/MainActivity.java
+++ app/src/main/java/com/example/zadaniezdjeciejava/MainActivity.java
@@ -8,10 +8,59 @@ import android.widget.SeekBar;
 
 public class MainActivity extends AppCompatActivity {
 
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        final int[] RValueInt = {0};
+        final int[] GValueInt = {0};
+        final int[] BValueInt = {0};
+
+        SeekBar rSeekBar = findViewById(R.id.RSeekBar);
+        ImageView zdj = findViewById(R.id.picture);
+        SeekBar Transparency = findViewById(R.id.Transparency);
+
+        Transparency.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+            @Override
+            public void onProgressChanged(SeekBar seekBar, int i, boolean b) {
+                // obliczanie przezroczystosci
+                int x = i/10;
+                zdj.setImageAlpha(x);
+            }
+
+            @Override
+            public void onStartTrackingTouch(SeekBar seekBar) {
+
+            }
+
+            @Override
+            public void onStopTrackingTouch(SeekBar seekBar) {
+
+            }
+        });
+
+        rSeekBar.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+            @Override
+            public void onProgressChanged(SeekBar seekBar, int i, boolean b) {
+                RValueInt[0] = i;
+                ZmianaKoloru();
+            }
+
+            @Override
+            public void onStartTrackingTouch(SeekBar seekBar) {
+
+            }
+
+            @Override
+            public void onStopTrackingTouch(SeekBar seekBar) {
+
+            }
+        });
+
+    }
+
+    private void ZmianaKoloru() {
     }
 }
