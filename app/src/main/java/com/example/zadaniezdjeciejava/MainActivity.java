086275a8fe2e912e75217c7368e76416af9fcf17
Kurtyka03 <kurtyka03@gmail.com>
2022-10-24 15:56:38 +0200

    pokazanie wartosci seekbara

--- app/src/main/java/com/example/zadaniezdjeciejava/MainActivity.java
+++ app/src/main/java/com/example/zadaniezdjeciejava/MainActivity.java
@@ -5,6 +5,7 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import android.widget.ImageView;
 import android.widget.SeekBar;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -19,8 +20,14 @@ public class MainActivity extends AppCompatActivity {
         final int[] BValueInt = {0};
 
         SeekBar rSeekBar = findViewById(R.id.RSeekBar);
+        SeekBar gSeekBar = findViewById(R.id.GSeekBar);
+        SeekBar bSeekBar = findViewById(R.id.BSeekBar);
         ImageView zdj = findViewById(R.id.picture);
         SeekBar Transparency = findViewById(R.id.Transparency);
+
+        TextView rValue = findViewById(R.id.RValue);
+        TextView gValue = findViewById(R.id.GValue);
+        TextView bValue = findViewById(R.id.BValue);
 
         Transparency.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
             @Override
@@ -44,8 +51,7 @@ public class MainActivity extends AppCompatActivity {
         rSeekBar.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
             @Override
             public void onProgressChanged(SeekBar seekBar, int i, boolean b) {
-                RValueInt[0] = i;
-                ZmianaKoloru();
+                rValue.setText(Integer.toString(i));
             }
 
             @Override
@@ -59,6 +65,39 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
+        gSeekBar.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+            @Override
+            public void onProgressChanged(SeekBar seekBar, int i, boolean b) {
+                gValue.setText(Integer.toString(i));
+            }
+
+            @Override
+            public void onStartTrackingTouch(SeekBar seekBar) {
+
+            }
+
+            @Override
+            public void onStopTrackingTouch(SeekBar seekBar) {
+
+            }
+        });
+
+        bSeekBar.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+            @Override
+            public void onProgressChanged(SeekBar seekBar, int i, boolean b) {
+                bValue.setText(Integer.toString(i));
+            }
+
+            @Override
+            public void onStartTrackingTouch(SeekBar seekBar) {
+
+            }
+
+            @Override
+            public void onStopTrackingTouch(SeekBar seekBar) {
+
+            }
+        });
     }
 
     private void ZmianaKoloru() {
